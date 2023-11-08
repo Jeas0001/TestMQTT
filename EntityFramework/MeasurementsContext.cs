a216470b5eea700f4e9ed1b59112645209bd5f8a
Jeas0001 <jeas0001@edu.mercantec.dk>
2023-11-08 14:34:45 +0100

    EntityFramework

---
+++ EntityFramework/MeasurementsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using MySql.EntityFrameworkCore.Extensions;
+
+namespace EntityFramework
+{
+    public class MeasurementsContext : DbContext
+    {
+        public DbSet<Measurements> measurements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+        }
+    }
+}
